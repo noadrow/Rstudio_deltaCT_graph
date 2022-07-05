2de903fe2730b90f3c76fad4881e70580bbc8581
botnet <botnet@botnet.tau.ac.il>
2022-07-05 11:28:27 +0300

    add finished python script

--- README.md
+++ README.md
@@ -1,4 +1,31 @@
 # Description 
-plot values and standart deviastion 
+This script calculate mean values and standart deviation for each sample and condition
+Then it calculate the difference to the control condition for each sample
+and plot the results
 
+## Required:
+1. python3
+2. python pacakes: numpy, mathplotlib, pandas
 
+# input 
+The first argument to run the script is the file location\name
+the second argument is the control condition name
+the rest of the argument are all the other conditions
+
+# NOTE: 
+It's important to keep the notation of condition the same as in the "Target name" column
+
+## Example:
+for example I added the file called example_table.csv 
+you can run the following command in the terminal: 
+ 
+```bash
+python graph_deltaCT.py example_table.csv "un-cut (C)" "diluted 1:2 (D)" "cut (M)"
+```
+
+format:
+python graph_deltaCT.py "file path" "control condition" ,,, "other conditions"
+
+## output:  
+for each sample (collected by "sample Name" column) 
+the script ouput a graph with a difference value for each condition to control condition
--- graph_deltaCT.py
+++ graph_deltaCT.py
@@ -1,18 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
+import sys
+
+# This script calculate mean values and standart deviation for each sample and condition
+# Then it calculate the difference to the control condition for each sample
+# and plot the results
+
+# The first argument to run the script is the file location\name
+# the second argument is the control condition name
+# the rest of the argument are all the other conditions
+# NOTE: It's important to keep the notation of condition the same as in the "Target name" column
 
 def mean_std_cal(filepath):
        file_df = pd.read_csv(filepath, sep=",", header=0)
-       Sample_Name = file_df["Sample Name"].unique()
+       samples = file_df["Sample Name"].unique()
        Condition_Name = file_df["Target Name"].unique()
        cal_df = pd.DataFrame()
        columns = list(cal_df)
 
-       for Sample in Sample_Name:
+       for Sample in samples:
               for Condition in Condition_Name:
                      mean = file_df.loc[(file_df["Sample Name"] == Sample)
-                                        & (file_df["Target Name"] == Condition)]["Cт"].mean
+                                        & (file_df["Target Name"] == Condition)]["Cт"].mean()
                      std = file_df.loc[(file_df["Sample Name"] == Sample)
                                        & (file_df["Target Name"] == Condition)]["Cт"].std()
 
@@ -25,63 +35,75 @@ def mean_std_cal(filepath):
 
                      cal_df = cal_df.append([temp_dict], ignore_index=True)
        cal_df = cal_df.dropna()
+       return cal_df, samples
 
-'''
-def diff_cal(cal_df):
-       for sample in cal_df["sample"]:
-              sample["diluted 1:2 (D)"]
-              sample["cut (M)"]
-              sample["un-cut (C)"]
-'''
+def diff_cal(cal_df,conditions,samples):
+       results_df = pd.DataFrame()
+       for sample in samples:
+              for i,condition in enumerate(conditions):
+                     if (i==0):
+                            Csample_mean = cal_df.loc[(cal_df["sample"] == sample) &
+                                                      (cal_df["condition"] == conditions[0])]['mean']
+                            Csample_std = cal_df.loc[(cal_df["sample"] == sample) &
+                                                     (cal_df["condition"] == conditions[0])]['std']
+                     else:
+                            conditionValue_mean = cal_df.loc[(cal_df["sample"] == sample) &
+                                                             (cal_df["condition"] == condition)]['mean']
+                            conditionValue_std = cal_df.loc[(cal_df["sample"] == sample) &
+                                                            (cal_df["condition"] == condition)]['std']
+
+                            diff_mean = float(conditionValue_mean) - float(Csample_mean)
+                            diff_std = float(Csample_std) + float(conditionValue_std)
+
+                            temp_dict = {
+                                   "sample": sample,
+                                   "condition": condition,
+                                   "diff_mean": diff_mean,
+                                   "diff_std": diff_std
+                            }
+
+                            results_df = results_df.append([temp_dict], ignore_index=True)
+       return results_df
+
+def plot_CT(samples,conditions,results_df):
+       for i,sample in enumerate(samples):
+              x_pos = np.arange(len(conditions) - 1)
+
+              CTEs = results_df.loc[results_df["sample"]==sample]["diff_mean"].values.tolist()
+              error = results_df.loc[results_df["sample"]==sample]["diff_std"].values.tolist()
+
+              # Build the plot
+              fig, ax = plt.subplots()
+              ax.bar(x_pos,
+                     CTEs,
+                     yerr=error,
+                     align='center',
+                     alpha=0.5,
+                     ecolor='black',
+                     color='black',
+                     capsize=10)
+
+              ax.set_ylabel("delta_CT")
+              ax.set_xticks(x_pos)
+              ax.set_xticklabels(conditions[1:])
+              ax.set_title(sample+" Difference To Control")
+              ax.yaxis.grid(True)
+
+              # Save the figure and show
+              plt.tight_layout()
+              plt.savefig(sample+'_bar_plot_with_error_bars.png')
+              plt.show()
 
 
-#variables preperation
-filepath = 'example_table.csv'
-Title = 'title'
-Y_lable = 'y-Lable'
-mean_std_cal(filepath)
+#Variables Preparation
+filepath = sys.argv[1]
+#NOTE: control should be the first condition
+conditions = sys.argv[2:]
+#example_table.csv "un-cut (C)" "diluted 1:2 (D)" "cut (M)"
 
-# Enter raw data
-aluminum = np.array([6.4e-5 , 3.01e-5 , 2.36e-5, 3.0e-5, 7.0e-5, 4.5e-5, 3.8e-5, 4.2e-5, 2.62e-5, 3.6e-5])
-copper = np.array([4.5e-5 , 1.97e-5 , 1.6e-5, 1.97e-5, 4.0e-5, 2.4e-5, 1.9e-5, 2.41e-5 , 1.85e-5, 3.3e-5 ])
-steel = np.array([3.3e-5 , 1.2e-5 , 0.9e-5, 1.2e-5, 1.3e-5, 1.6e-5, 1.4e-5, 1.58e-5, 1.32e-5 , 2.1e-5])
-
-# Calculate the average
-Aluminum_mean = np.mean(aluminum)
-Copper_mean = np.mean(copper)
-Steel_mean = np.mean(steel)
-
-
-# Calculate the standard deviation
-aluminum_std = np.std(aluminum)
-copper_std = np.std(copper)
-steel_std = np.std(steel)
-
-# Create lists for the plot
-materials = ['Aluminum', 'Copper', 'Steel']
-x_pos = np.arange(len(materials))
-CTEs = [Aluminum_mean, Copper_mean, Steel_mean]
-error = [aluminum_std, copper_std, steel_std]
-
-# Build the plot
-fig, ax = plt.subplots()
-ax.bar(x_pos,
-       CTEs,
-       yerr=error,
-       align='center',
-       alpha=0.5,
-       ecolor='black',
-       color='black',
-       capsize=10)
-ax.set_ylabel(Y_lable)
-ax.set_xticks(x_pos)
-ax.set_xticklabels(materials)
-ax.set_title(Title)
-ax.yaxis.grid(True)
-
-# Save the figure and show
-plt.tight_layout()
-plt.savefig('bar_plot_with_error_bars.png')
-plt.show()
-
-
+#create mean and standart deviation for duplecates
+cal_df,samples = mean_std_cal(filepath)
+#create difference of mean value to control
+results_df = diff_cal(cal_df,conditions,samples)
+#plot for each sample
+plot_CT(samples,conditions,results_df)
